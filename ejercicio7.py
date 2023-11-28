d82624307b960f2f9fa215772a900ee1e02bacd9
watydiaz <40471433+watydiaz@users.noreply.github.com>
2023-11-28 10:44:56 -0500

    Ejercicio 7 hecho

--- ejercicio7.py
+++ ejercicio7.py
@@ -2,3 +2,12 @@
 7 - Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras que tienen el mismo aspecto escritas invertidas), 
 ejemplo: es_palindromo ("radar") tendría que devolver True.
 '''
+nombre = input ('Ingrese su nombre: ')
+cadena = input ('Ingrese la palabra que quiere verificar: ')
+
+def es_palindromo(cadena):
+    cadena = cadena.lower()
+    palabra_invertida = cadena[::-1]
+    return cadena == palabra_invertida
+
+print (f'la palabara {cadena}, es palindroma: {es_palindromo(cadena)}')
